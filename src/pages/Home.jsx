6a8bf05bc1696c0d543446e68a49bbaaeb4dca0e
csaradev <aranda.chs@gmail.com>
2022-08-31 22:04:55 +0200

    home built

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../styles/Home.scss';
+
 
 const Home = () => {
 	return (
-		<div className="Login">
-			<h1 className="title">hi</h1>
+		<div className="Home">
+			<h1 className="wellcome-msg">Welcome to <a className='wellcome-msg-yard'>YARD</a> Sale</h1>
 				
 		</div >
 	);
