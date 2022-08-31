404a1cb871f5e591aabde27f33cd36e522e51fd5
csaradev <aranda.chs@gmail.com>
2022-08-31 14:55:58 +0200

    cleaner index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './routes/App';
-
-ReactDOM.render(<App />, document.getElementById('app'));
-*/
 
 import App from './routes/App';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(<App />);
