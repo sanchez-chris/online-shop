67540acf6a45962b5ad9d2957baa0bd0b5499d4a
csaradev <aranda.chs@gmail.com>
2022-08-31 17:33:25 +0200

    msg

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -28,25 +28,3 @@ const OrderItem = (props) => {
 
 export default OrderItem;
 
-/*
-const OrderItem = ({ product }) => {
-	const { removeFromCart } = useContext(AppContext);
-
-	const handleRemove = product => {
-		removeFromCart(product);
-	}
-
-	return (
-		<div className="OrderItem">
-			<figure>
-				<img src={product.images[0]} alt={product.title} />
-			</figure>
-			<p>{product.title}</p>
-			<p>${product.price}</p>
-			<img src={close} alt="close" onClick={() => handleRemove(product)} />
-		</div>
-	);
-}
-
-export default OrderItem;
-*/
