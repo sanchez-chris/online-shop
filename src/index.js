6dfa67f391e9fb63708c098893d8a9099c7b3f56
csaradev <aranda.chs@gmail.com>
2022-09-02 17:27:47 +0200

    msg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import App from './routes/App';
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App />);
+
+ReactDOM.render(
+  <>
+      <App />
+  </>,
+  document.getElementById('app'),
+);
