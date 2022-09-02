4cec13edbe3b3ffb5a52910989bcc44cd945094f
csaradev <aranda.chs@gmail.com>
2022-09-02 17:44:58 +0200

    msg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import '../styles/Home.scss';
 const Home = () => {
 	return (
 		<div className="Home">
-			<h1 className="wellcome-msg">Welcome to <p className='wellcome-msg-yard'>YARD</p> Sale</h1>
+			<h1 className="wellcome-msg">Welcome to <strong className='wellcome-msg-yard'>YARD</strong> Sale</h1>
 				
 		</div >
 	);
